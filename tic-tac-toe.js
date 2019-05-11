0e4cb24b27e5e87513e1cf2c02e0f35b14d7a5f3
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2019-05-11 02:45:58 -0300

    make_play function and turns added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,40 @@
 const tic_tac_toe = {
 
-    board: ['','','','','','','','',''],
+    board: ["","","","","","","","",""],
     container_element: null,
+
+    symbol: {
+        options: ["X", "O"],
+        index: 0,
+
+        turn: function(){
+            let turn = this.options[this.index];
+            this.turn_change();
+            return turn;
+        },
+
+        turn_change: function(){
+            if (this.index === 0 ? this.index = 1 : this.index = 0);
+        }
+    },
 
     init: function(container){
         this.container_element = container;
         this.draw();
     },
 
+    make_play: function(position){
+        this.board[position] = this.symbol.turn();
+        this.draw();
+    },
+
     draw: function(){
+        let content = "";
+
         for (let i in this.board){
-            this.container_element.innerHTML += `<div>${this.board[i]}</div>`;
+            content += `<div onclick="tic_tac_toe.make_play(${i})">${this.board[i]}</div>`;
         }
+
+        this.container_element.innerHTML = content;
     }
 };
