1e33d9e7ab1761af5095d933b707edbf47526a18
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2019-05-11 02:35:49 -0300

    Init and Draw functions added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,16 @@
-const tic_tac_toe = {};
+const tic_tac_toe = {
+
+    board: ['','','','','','','','',''],
+    container_element: null,
+
+    init: function(container){
+        this.container_element = container;
+        this.draw();
+    },
+
+    draw: function(){
+        for (let i in this.board){
+            this.container_element.innerHTML += `<div>${this.board[i]}</div>`;
+        }
+    }
+};
