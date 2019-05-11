560597572ee83bf1153ef1cd566b31a03f84c637
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2019-05-11 02:51:11 -0300

    Winning sequences added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,6 @@
 const tic_tac_toe = {
 
     board: ["","","","","","","","",""],
-    container_element: null,
-
     symbol: {
         options: ["X", "O"],
         index: 0,
@@ -17,6 +15,19 @@ const tic_tac_toe = {
             if (this.index === 0 ? this.index = 1 : this.index = 0);
         }
     },
+
+    winning_sequences: [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ],
+    
+    container_element: null,
 
     init: function(container){
         this.container_element = container;
